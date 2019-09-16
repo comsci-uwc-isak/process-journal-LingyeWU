357e0ed222884483b556f4e75ba9e0bc74d053c5
LingyeWU <2021.lingye.wu@uwcisak.jp>
2019-09-16 11:33:16 +0900

    uploading hmw to the cloud

---
+++ BASH/Class6_Sep.11th/HMW/average.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Welcome to the Running Average"
+
+valid=true
+sum=0
+n=0
+
+while [ $valid ]
+do
+
+	echo "Enter a number or Exit (X): "
+	read num
+	if [ $num == x ]; then
+		break
+
+	else
+	(( sum=num+sum ))
+	(( n++ ))
+	(( ave=sum/n ))
+	echo "Running average is: "$ave
+fi
+
+done
+
+echo "exiting"
+exit
