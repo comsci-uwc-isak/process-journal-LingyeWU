99157272345cbec2fb18a2d5a07a98adf9d8416b
LingyeWU <54606950+LingyeWU@users.noreply.github.com>
2019-09-16 11:29:49 +0900

    Delete Class5.sh

--- Class5.sh
+++
@@ -1,8 +0,0 @@
-Two key takeaways from today's class:
-
-a) Today we spent most of our time working on the game. It was, to be honest, a lot of
-frustration, because while using a lot of "if...then" commands, a lot of time it just does
-not work well. Then I tried to use case command which made it much more easier.
-
-b) Another takeaway is that we learnt how to update our local documents to the cloud,
-which was super useful.
